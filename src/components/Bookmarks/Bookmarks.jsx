87ece2c6a399902d2a403dd6aab4247142abb255
MdIsrakulIslam <israkulislam@gmail.com>
2025-01-14 01:01:42 +0600

    add full

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,17 @@
+import Bookmark from "../Bookmark/Bookmark";
+
+const Bookmarks = ({bookmarks,readingTime}) => {
+    return (
+        <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 p-4">
+            <div>
+                <h3 className="text-4xl">Reading Time:{readingTime}</h3>
+            </div>
+            <h2 className="3xl text-center ">Bookmarked Blogs:{bookmarks.length}</h2>
+            {
+                bookmarks.map(bookmark=><Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+            }
+        </div>
+    );
+};
+
+export default Bookmarks;
